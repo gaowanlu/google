b2865b1b814d9674b79a642360c874973c90fe92
yuwei <yuwei@andunip.com>
2023-04-15 09:06:44 +0800

    改为openai试试

--- api/proxy.js
+++ api/proxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";
+  let target = "https://api.openai.com";
   // 代理目标地址
   // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
   //   if (
