d277cd61a28f7c4bbbf0efdf7fc8a3893815ec5c
wanlu <2209120827@qq.com>
2023-04-30 01:51:40 +0800

    Update proxy.js
    comment

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = (req, res) => {
-  let target = "https://www.google.com/";
-  // 代理目标地址
-  // 这里使用 backend 主要用于区分 vercel serverless 的 api 路径
+  let target = "https://www.google.com/";//your website url
   //   if (
   //     req.url.startsWith("/api") ||
   //     req.url.startsWith("/auth") ||
@@ -13,13 +11,12 @@ module.exports = (req, res) => {
   //     target = "http://106.15.2.32:6969";
   //   }
 
-  // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      // 通过路径重写，去除请求路径中的 `/backend`
-      // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+      // rewrite request path `/backend`
+      //  /backend/user/login => http://google.com/user/login
       //   "^/backend/": "/",
     },
   })(req, res);
